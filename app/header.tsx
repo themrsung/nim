0c358ca7e58e865ced4dcee821cfceda90aca68e
Min Jun Sung <themrsung@gmail.com>
2025-09-11 08:08:24 +0900

    names

--- app/header.tsx
+++ app/header.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { TextEffect } from '@/components/ui/text-effect'
+import { TextLoop } from '@/components/ui/text-loop'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -13,10 +14,13 @@ export function Header() {
         <div style={{ height: "30px" }} /> {/* 30px empty space added */}
         <Link href="/" className="font-bold text-black dark:text-white">
           서준
-        </Link>
-        <TextEffect className="font-medium text-zinc-600 dark:text-zinc-300">
-          Min Jun "Mark" Sung - 성민준
-        </TextEffect>
+        </Link> | {" "}
+        <TextLoop className="font-medium text-zinc-600 dark:text-zinc-300">
+          <span>Min Jun "Mark" Sung</span>
+          <span>성민준</span>
+          <span>sjun___</span>
+          <span>themrsung</span>
+        </TextLoop>
         <TextEffect
           as="p"
           preset="fade"
